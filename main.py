4cef7e9e0ff0130437608c5251f108f9eda0f663
徐英杰 <806895158@qq.com>
2025-03-14 16:58:27 +0800

    Add new feature

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from data.plugins.astrbot_plugin_beautiful_giri.api_collection import video
 
-@register("美女集合", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("美女集合", "黄四郎", "美女集合", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
